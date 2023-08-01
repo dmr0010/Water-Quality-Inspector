018d109590db8dc93976e8fee7821186a29951ba
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-08-01 18:31:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,6 @@
 # Importing the necessary Python modules.
 import streamlit as st
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 
 # Import necessary functions from web_functions
 from web_functions import load_data
